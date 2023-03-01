c0d9c6c4f6a99d248be846d249148d0800d64cd4
dvd092 <dvd092bhbn@yahoo.co.jp>
2023-03-01 14:22:16 +0900

    途中保尊２

--- app/models/articles.go
+++ app/models/articles.go
@@ -3,7 +3,7 @@ package models
 import (
 	// "encoding/json"
 	// "fmt"
-	"log"
+	// "log"
 	"time"
 	// "net/http"
 	// gorm mysql
@@ -24,20 +24,7 @@ type Article struct {
 
 
 func GetArticles() (arts []Article, err error) {
-	cmd := `select id, user_ex_id, categories_id, title, plot, likes, created_at from articles`
-	rows, err := Db.Query(cmd)
-		if err!= nil {
-      log.Fatalln(err)
-    }
-		for rows.Next() {
-			var art Article
-			err = rows.Scan(&art.ID, &art.UserExID, &art.CategoryId, &art.Title, &art.Plot, &art.Likes, &art.CreatedAt)
-		if err!= nil {
-      log.Fatalln(err)
-		}
-		arts = append(arts,art)
-	}
-	rows.Close()
+	DB.Order("likes desc").Find(&arts)
 	return arts, err
 }
 
