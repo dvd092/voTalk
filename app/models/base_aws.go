2b214012ca95fb34ec60e744ae1e8639a551a6e1
dvd092 <67625361+dvd092@users.noreply.github.com>
2023-03-10 01:23:42 +0900

    Add files via upload

---
+++ app/models/base_aws.go
@@ -0,0 +1,85 @@
+package models
+
+import (
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	"log"
+	"votalk/config"
+	// gorm mysql
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+var Db *sql.DB
+var DB *gorm.DB
+
+var err error
+
+const (
+	tableNameExUser  = "ex_users"
+	tableNameVwUser  = "vw_users"
+	tableNameSession = "sessions"
+)
+
+func init() {
+	Db, err = sql.Open(config.Config.SQLDriver, "ex_po_user:kdash@tcp(votalk-database.c1ifgotpjxxn.ap-northeast-1.rds.amazonaws.com:3306)/ex_po?parseTime=true")
+	DB, err = gorm.Open("mysql", "ex_po_user:kdash@tcp(votalk-database.c1ifgotpjxxn.ap-northeast-1.rds.amazonaws.com:3306)/ex_po?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = Db.Ping()
+	if err != nil {
+		fmt.Println("接続失敗")
+	} else {
+		fmt.Println("接続成功")
+	}
+
+	//viewユーザーテーブル作成
+	cmdVwU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY AUTO_INCREMENT,uuid VARCHAR(100),name VARCHAR(100),email VARCHAR(100),password VARCHAR(100),created_at DATETIME)`, tableNameVwUser)
+
+	_, err = Db.Exec(cmdVwU)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Db.Exec(cmdVwU)
+
+	//expertユーザーテーブル作成
+	cmdExU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY AUTO_INCREMENT,uuid VARCHAR(100),name VARCHAR(100),email VARCHAR(100),password VARCHAR(100),created_at DATETIME)`, tableNameExUser)
+
+	_, err = Db.Exec(cmdExU)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Db.Exec(cmdExU)
+
+	// cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY AUTO_INCREMENT,content TEXT, user_id INTEGER, created_at DATETIME)`, tableNameTodo)
+
+	// Db.Exec(cmdT)
+
+	//セッションテーブル作成
+	cmdS := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
+		id INTEGER PRIMARY KEY AUTO_INCREMENT,
+		uuid VARCHAR(255) NOT NULL UNIQUE,
+		email VARCHAR(255),
+		user_id INTEGER,
+		user_type INTEGER,
+		created_at DATETIME)`, tableNameSession)
+
+	Db.Exec(cmdS)
+
+}
+
+func createUUID() (uuidobj uuid.UUID) {
+	uuidobj, _ = uuid.NewUUID()
+	return uuidobj
+}
+
+func Encrypt(plaintext string) (cryptext string) {
+	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return cryptext
+}
